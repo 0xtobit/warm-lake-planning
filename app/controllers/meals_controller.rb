ff27faa70ae12553eadbbea68bc4481267a8b56d
Tobit Raff <0xtobit@gmail.com>
2018-04-19 20:09:26 -0600

    Move default route from trips#show to meals#index

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,5 +1,28 @@
 class MealsController < ApplicationController
-  before_action :set_meal
+  before_action :set_meal, only: [:edit, :update]
+
+  def index
+    trip = Trip.find(1)
+    @days = (0..trip.num_days-1).map do |day_index|
+      breakfast = Meal.breakfast.find_by(trip: trip, day_index: day_index)
+      dinner = Meal.dinner.find_by(trip: trip, day_index: day_index)
+      day = {
+        day: dinner&.day_index,
+        date: (trip.first_day + day_index.days).strftime('%a %b %d'),
+        breakfast: breakfast&.menu,
+        breakfast_host: breakfast&.host,
+        breakfast_attendance: Attendance.pluck("day#{day_index}").transpose[0]&.map(&:to_i)&.sum || 0,
+        dinner: dinner&.menu,
+        dinner_host: dinner&.host,
+        dinner_attendance: Attendance.pluck("day#{day_index}").transpose[2]&.map(&:to_i)&.sum || 0,
+        breakfast_open: breakfast&.host.nil?,
+        dinner_open: dinner&.host.nil?
+      }
+      day[:breakfast_link] = edit_meal_path(breakfast) if breakfast
+      day[:dinner_link] =  edit_meal_path(dinner) if dinner
+      day
+    end
+  end
 
   def edit
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'trip/show'
-
-  root to: 'trip#show'
-  #root to: 'meals#index'
+  root to: 'meals#index'
 
   resources :attendances, only: [:new, :create]
   resources :meals, only: [:edit, :update]
